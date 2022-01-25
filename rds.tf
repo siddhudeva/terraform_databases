2ea041bc81d5782efb393395d8b5d31cc487dc3b
siddhu <siddhudangeti@gmail.com>
2022-01-25 22:12:45 +0530

    rds which is mysql database is updated

--- rds.tf
+++ rds.tf
@@ -19,24 +19,3 @@ resource "aws_db_subnet_group" "mysqlsubnet" {
     Name = "mysql_${var.ENV}-user"
   }
 }
-resource "aws_db_security_group" "mysql" {
-  name = "mysql-${var.ENV}"
-
-  ingress {
-    description = "MysqlDB"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = concat(data.terraform_remote_state.vpc.outputs.PRIVATE_CIDR, tolist([data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]))
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  /// as we know all outbound rules are open to internet
-  tags = {
-    Name = "${var.ENV}-sg-Mysql"
-  }
-}
