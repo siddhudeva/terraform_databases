b68744e9b80d45f9193d019774ac436636396665
siddhu <siddhudangeti@gmail.com>
2022-02-07 16:17:05 +0530

    sg group updated

--- module/ec2-db/db-deploy.tf
+++ module/ec2-db/db-deploy.tf
@@ -5,17 +5,22 @@ resource "null_resource" "db-deploy" {
     instance_ids = aws_spot_instance_request.DB.spot_instance_id
   }
   provisioner "remote-exec" {
-  connection { /////using provided connection details like credentials which is going to login and execute shell commands
-    type     = "ssh"
-    user     = local.SSH_USERNAME
-    password = local.SSH_PASSWORD
-    host     = aws_spot_instance_request.DB.private_ip
-  }
+    connection { /////using provided connection details like credentials which is going to login and execute shell commands
+      type     = "ssh"
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
+      host     = aws_spot_instance_request.DB.private_ip
+    }
 
     inline = [
       "ansible-pull -U https://github.com/siddhudeva/ansible-1.git roboshop-pull.yml -e COMPONENT=${var.DB_COMPONENT} -e ENV=${var.ENV}"
     ]
   }
+}
+
+locals {
+  SSH_USERNAME = var.SSH_USERNAME
+  SSH_PASSWORD = var.SSH_PASSWORD
 }
 
 locals {
